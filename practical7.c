aba253445edfac5f4582d6565f7d2e83cb405d03
Amit Sandhu <amiteshwarsinghlps3429@gmail.com>
2026-02-27 11:11:42 +0530

    Update practical7.c

--- practical7.c
+++ practical7.c
@@ -4,11 +4,12 @@
 int main() {
     int a,b,temp;
     a = 5;
-    b = 10;
-    printf("B")
+    b = 10; 
+    printf("B")                                       //2510990027
     temp = a;
     a = b;
     b = temp;
     printf("Swapped var: a = %d, b = %d",a,b);
     return 0;
+
 }
