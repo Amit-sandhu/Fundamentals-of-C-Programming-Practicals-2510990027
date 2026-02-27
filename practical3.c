7e81205400456a401bb43fc794dfca3a45f7e7e8
Amit Sandhu <amiteshwarsinghlps3429@gmail.com>
2026-02-27 11:10:05 +0530

    Update practical3.c

--- practical3.c
+++ practical3.c
@@ -5,6 +5,7 @@ int main() {
     printf("Enter 2 nums space separated: ");
     scanf("%d %d",&a,&b);
 
-    printf("The sum of %d + %d = %d",a,b,a+b);      //2510990119
+    printf("The sum of %d + %d = %d",a,b,a+b);      //2510990027
     return 0;
+
 }
